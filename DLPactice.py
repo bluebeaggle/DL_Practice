96692e5745077f0ddc5c0f3775575670c44f231c
daeheon <bluebeaggle@google-server.us-central1-f.c.awesome-griffin-345907.internal>
2022-04-01 13:01:48 +0000

    Ubuntu_Commit

--- DLPactice.py
+++ DLPactice.py
@@ -6,4 +6,18 @@ def AND(x1,x2) :
     elif tmp > theta :
         return 1;
 
-AND(0,0)
+print(AND(0,0))
+print(AND(0,1))
+print(AND(1,0))
+print(AND(1,1))
+
+import numpy as np
+
+x = np.array([0,1])
+w = np.array([0.5,0.5])
+b = -7
+
+print(w*x)
+print(np.sum(w*x))
+print(np.sum(w*x)+b)
+
