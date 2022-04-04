95975c1624198e2fe8246fbbaa796aa04eaa0b02
daeheon <shew1903@gmail.com>
2022-04-04 06:51:40 +0000

    Matrix Multiplication

--- DLPractice.py
+++ DLPractice.py
@@ -16,5 +16,13 @@ B = np.array([[5,6],[7,8]])
 print('B.shape = ',B.shape)
 print('np.dot(A,B) = ' , np.dot(A,B))
 
+X = np.array([1,2])
+print('X.shape = ', X.shape)
+W = np.array([[1,3,5],[2,4,6]])
+print('W = ', W)
+print('W.shape = ', W.shape)
+Y = np.dot(X,W)
+print('Y = np.dot(X,W) ')
+print(Y)
 
 
