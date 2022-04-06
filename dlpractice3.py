de20ffd6d5e55f96fcd2926800ceab851b43a542
daeheon <shew1903@gmail.com>
2022-04-06 08:53:45 +0000

    get_data

--- dlpractice3.py
+++ dlpractice3.py
@@ -1,8 +1,14 @@
 import sys, os
 import numpy as np
-
 sys.path.append(os.pardir)
 from dataset.mnist import load_mnist
+from PIL import Image
+
+
+
+def img_show(img) :
+    pil_img = Image.fromarray(np.unit8(img))
+    pil_img.show()
 
 
 (x_train, t_train), (x_test, t_test) =\
@@ -13,5 +19,42 @@ print(x_train.shape)
 print(t_train.shape)
 print(x_test.shape)
 print(t_test.shape)
+
+img = x_train[0]
+label = t_train[0]
+print(label)
+
+print(img.shape)
+img = img.reshape(28,28)
+print(img.shape)
+
+img_show(img)
+
+
+def get_data():
+    (x_train, t_train),(x_test,t_test) =\
+            load_mnist(normalize=True, flatten=True, one_hot_label=False)
+    return x_test, t_test
+
+def init_network():
+    with open("sample_weight.pkl",'rb') as f:
+        network = picle.load(f)
+
+    return network
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'],network['W2'],network['W3']
+    b1, b2, b3 = network['b1'],network['b2'],network['b3']
+
+    a1 = np.dot(x,W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1,W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2,W3) + b3
+    y = softmax(a3)
+
+    return y
+
+
 
 
