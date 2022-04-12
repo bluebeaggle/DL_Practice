2e80f71e05b1e0497f2052de8116858794b627a9
root <root@google-server.us-central1-f.c.awesome-griffin-345907.internal>
2022-04-12 12:21:14 +0000

    get_data()

--- dlpractice3.py
+++ dlpractice3.py
@@ -57,4 +57,15 @@ def predict(network, x):
 
 
 
+x,y = get_data()
+network = init_network()
+
+accuracy_cnt = 0
+for i in range(len(x)):
+    y = predict(network, x[i])
+    p = np.argmax(y)
+    if p == t[i] :
+        accuracy_cnt += 1
+
+print("Accuracy:"+str(float(accuracy_cnt) / len(x)))
 
